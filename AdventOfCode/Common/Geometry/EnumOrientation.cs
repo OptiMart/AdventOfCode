ffe4ebed8c1cd45de4de152c1a8f39d051776413
Martin Strobl <m.strobl@outlook.at>
2022-12-12 22:59:04 +0100

    Cleanup

--- AdventOfCode/Common/Geometry/EnumOrientation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AoC.AdventOfCode.Common.Geometry
-{
-    public enum EnumOrientation
-    {
-        General = 0,
-        Horizontal = 1,
-        Vertical = 2,
-    }
-}
--- PS_Invoke-Webrequest.ps1
+++
@@ -1,9 +0,0 @@
-﻿$session = [Microsoft.PowerShell.Commands.WebRequestSession]::new()
-
-$cookie = [System.Net.Cookie]::new('session', '')
-$session.Cookies.Add('https://adventofcode.com', $cookie)
-
-$input = Invoke-WebRequest 'https://adventofcode.com/2019/day/1' -WebSession $session
-
-
-$response = Invoke-WebRequest 'https://adventofcode.com/2022/day/3/answer' -Body @{level = "1"; answer = "8298"} -WebSession $session -Method Post
